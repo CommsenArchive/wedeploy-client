c3c99e795b93a4334f17ad074099c8f2f3fb8612
Milen Dyankov <milendyankov@gmail.com>
2017-08-27 01:06:21 +0200

    move test to `client APIs` module

--- java-client/com.commsen.wedeploy.data.mapper.gson/src/test/java/com/commsen/wedeploy/data/mapper/gson/JsonMapperTest.java
+++ java-client/com.commsen.wedeploy.data.mapper.gson/src/test/java/com/commsen/wedeploy/data/mapper/gson/JsonMapperTest.java
@@ -1,108 +1,13 @@
-package com.commsen.wedeploy.client;
+package com.commsen.wedeploy.data.mapper.gson;
 
-import java.util.Date;
-import java.util.UUID;
+import com.commsen.wedeploy.client.data.AbstractJsonMapperTest;
+import com.commsen.wedeploy.data.mapper.gson.GsonJsonMapper;
 
-import org.junit.Test;
+public class JsonMapperTest extends AbstractJsonMapperTest {
 
-import com.commsen.wedeploy.client.data.WeDeployDataDocument;
-import com.commsen.wedeploy.client.data.WeDeployDataMapper;
-import com.commsen.wedeploy.mapper.gson.GsonJsonMapper;
-
-import org.junit.Assert;
-
-public class JsonMapperTest {
-
-	private WeDeployDataMapper jsonMapper = new GsonJsonMapper();
 			
-	@Test
-	public void test() {
-		
-		Item item = new Item();
-		String json = jsonMapper.toJson(item);
-		Assert.assertFalse(json.contains("\"id\":\"" + item.name + "\""));
-		Item result = jsonMapper.fromJson(json, Item.class);
-		Assert.assertEquals(item, result);
-		
-		WeDeployDataDocument<Item> itemDoc = new WeDeployDataDocument<Item>(item.name, item);
-		json = jsonMapper.toJson(itemDoc);
-		Assert.assertTrue(json.contains("\"id\":\"" + item.name + "\""));
-		result = jsonMapper.fromJson(json, Item.class);
-		Assert.assertEquals(item, result);
-		
-		itemDoc = new WeDeployDataDocument<Item>(null, item);
-		json = jsonMapper.toJson(itemDoc);
-		Assert.assertFalse(json.contains("\"id\":\"" + item.name + "\""));
-		result = jsonMapper.fromJson(json, Item.class);
-		Assert.assertEquals(item, result);
-
-		itemDoc = new WeDeployDataDocument<Item>("", item);
-		json = jsonMapper.toJson(itemDoc);
-		Assert.assertFalse(json.contains("\"id\":\"" + item.name + "\""));
-		result = jsonMapper.fromJson(json, Item.class);
-		Assert.assertEquals(item, result);
-
-		ItemWithId itemWithId = new ItemWithId();
-		json = jsonMapper.toJson(itemWithId);
-		Assert.assertFalse(json.contains("\"id\":\"" + itemWithId.name + "\""));
-		Assert.assertTrue(json.contains("\"id\":\"" + itemWithId.id + "\""));
-		ItemWithId resultWithId = jsonMapper.fromJson(json, ItemWithId.class);
-		Assert.assertEquals(itemWithId, resultWithId);
-		
-		item = new ItemWithId();
-		json = jsonMapper.toJson(item);
-		Assert.assertFalse(json.contains("\"id\":\"" + item.name + "\""));
-		Assert.assertTrue(json.contains("\"id\":\"" + ((ItemWithId)item).id + "\""));
-		resultWithId = jsonMapper.fromJson(json, ItemWithId.class);
-		Assert.assertEquals(item, resultWithId);
-		Assert.assertEquals((ItemWithId)item, resultWithId);
-		
-	}
-
-	static class ItemWithId extends Item {
-		String id =  UUID.randomUUID().toString();
-	}
-	
-	static class Item {
-		String name = UUID.randomUUID().toString();
-		Date date = new Date();
-		
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((date == null) ? 0 : date.hashCode());
-			result = prime * result + ((name == null) ? 0 : name.hashCode());
-			return result;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Item other = (Item) obj;
-			if (date == null) {
-				if (other.date != null)
-					return false;
-			} else if (!date.equals(other.date))
-				return false;
-			if (name == null) {
-				if (other.name != null)
-					return false;
-			} else if (!name.equals(other.name))
-				return false;
-			return true;
-		}
-
-		@Override
-		public String toString() {
-			return "Item [name=" + name + ", date=" + date + "]";
-		}
-	
+	public JsonMapperTest() {
+		jsonMapper = new GsonJsonMapper();
 	}
 		
 }
